b1b275d56960f6319bca019a038bd8974b9d3e92
Jason Che <chem9310@gmail.com>
2023-10-09 11:35:59 -0400

    Add article redirects in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import acceptLanguage from 'accept-language'
 import { fallbackLng, languages, cookieName } from './app/i18n/settings'
+import { redirects } from './src/data'
 
 acceptLanguage.languages(languages)
 
@@ -10,30 +11,35 @@ export const config = {
 }
 
 export function middleware(req: NextRequest) {
+
+    if (redirects[req.nextUrl.pathname]) {
+        return NextResponse.redirect(new URL(redirects[req.nextUrl.pathname], req.url))
+    }
+
     try {
         let lng
         //@ts-ignore
         if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value)
         if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'))
         if (!lng) lng = fallbackLng
-
+    
         // split path will split up to ['', 'locale', 'pathname']
         let splitPathname = req.nextUrl.pathname.split(`/`)
         
-        // Redirect to fallback locale with pathname appended for root visits /solutions => /en/solutions
-        if (splitPathname.length < 3 && !languages.some(loc => new RegExp((`^${loc}$`)).test(splitPathname[1]))) {
-            return NextResponse.redirect(new URL(`/${fallbackLng}${req.nextUrl.pathname}${req.nextUrl.search ? req.nextUrl.search : ``}`, req.url))
-        }
+        // // Redirect to fallback locale with pathname appended for root visits /solutions => /en/solutions
+        // if (splitPathname.length < 3 && !languages.some(loc => new RegExp((`^${loc}$`)).test(splitPathname[1]))) {
+        //     return NextResponse.redirect(new URL(`/${fallbackLng}${req.nextUrl.pathname}${req.nextUrl.search ? req.nextUrl.search : ``}`, req.url))
+        // }
         
         // Redirect if lng in path is not supported /de/solutions => /en/solutions
         if (
-            splitPathname.length > 2 &&
+            // splitPathname.length > 2 &&
             !languages.some(loc => new RegExp((`^${loc}$`)).test(splitPathname[1])) &&
             !req.nextUrl.pathname.startsWith('/_next')
         ) {
-            const removedLocale = splitPathname.splice(1, 1)
-            const purifiedPathname = splitPathname.join('/')
-            return NextResponse.redirect(new URL(`/${lng}${purifiedPathname}${req.nextUrl.search ? req.nextUrl.search : ``}`, req.url))
+            // const removedLocale = splitPathname.splice(1, 1)
+            // const purifiedPathname = splitPathname.join('/')
+            return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}${req.nextUrl.search ? req.nextUrl.search : ``}`, req.url))
         }
         
         if (req.headers.has('referer')) {
@@ -43,7 +49,7 @@ export function middleware(req: NextRequest) {
             if (lngInReferer) response.cookies.set(cookieName, lngInReferer)
             return response
         }
-
+    
         return NextResponse.next()
     } catch (error) {
         console.error('Middleware error:', error);
--- src/data/index.ts
+++ src/data/index.ts
@@ -9,3 +9,11 @@ export const solutionsStaticParams = [
         handle: `enterprise-web-app`,
     },
 ]
+
+export const redirects: { [key: string]: string } = {
+    '/blog/articles/boost-your-business-with-vps-the-smart-alternative-to-shared-hosting': '/blogs/business-technology-articles/boost-your-business-with-vps-the-smart-alternative-to-shared-hosting',
+    '/blog/articles/the-future-of-e-commerce-how-small-businesses-can-adapt-to-the-changing-landscape': '/blogs/business-technology-articles/the-future-of-e-commerce-how-small-businesses-can-adapt-to-the-changing',
+    '/blog/articles/marketing-in-the-digital-age-how-small-businesses-can-stand-out-online': '/blogs/business-technology-articles/marketing-in-the-digital-age-how-small-businesses-can-stand-out-online',
+    '/blog/articles/what-are-the-best-practices-for-securing-remote-work-for-business-employees': '/blogs/business-technology-articles/what-are-the-best-practices-for-securing-remote-work-for-business-employees',
+    '/blog/articles/top-5-pros-and-cons-of-automation-for-small-businesses': '/blogs/business-technology-articles/top-5-pros-and-cons-of-automation-for-small-businesses',
+}
